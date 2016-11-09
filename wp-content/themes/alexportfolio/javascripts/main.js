c9c9acdea483fea28a4c8e807b595ce02ba0df8e
Alex Tjokro <alex@tjok.ro>
2016-11-09 14:36:44 -0800

    Setup functions.php, added required JS and SCSS files

---
+++ wp-content/themes/alexportfolio/javascripts/main.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	// Please place all the responsive functions here
+	function respond() {
+    
+	}
+
+	$(document).ready(function() {
+		respond();
+		
+	});
+
+	$(window).load(function() {
+
+	});
+
+	$(window).resize(function() {
+		respond();
+		
+	});
+
+	$(window).scroll(function() {
+		
+	});
+
+}(jQuery))
